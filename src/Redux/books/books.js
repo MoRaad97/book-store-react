afd904a91468da5a4fdd1e915d551a2caae398de
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-07-18 12:58:54 +0300

    Change state to immutable in reducers

--- src/Redux/books/books.js
+++ src/Redux/books/books.js
@@ -3,21 +3,19 @@ const ADD_BOOK = 'book-store-react/books/ADD_BOOK';
 const REMOVE_BOOK = 'book-store-react/books/REMOVE_BOOK';
 
 // initial state
-const defaultState = {
-  bookList: [],
-};
+const bookList = []
 
 // Reducer
-export default function booksReducer(state = defaultState, action) {
+export default function booksReducer(state = bookList, action) {
   switch (action.type) {
     case ADD_BOOK:
-      return state.bookList.push({
+      return [...state, {
         title: action.title,
         author: action.bookAuthor,
         id: action.bookId,
-      });
+      }];
     case REMOVE_BOOK:
-      return state.bookList.filter((book) => book.id !== action.id);
+      return state.filter((book) => book.id !== action.id);
     default:
       return state;
   }
