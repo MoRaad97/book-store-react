b0b923a4cca18bcae80a7d45bd3470a37e5a355b
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-07-18 13:18:22 +0300

    Import Provider from react-redux & connect the store to it

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import store from './Redux/configureStore'
+import { Provider } from 'react-redux'
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
