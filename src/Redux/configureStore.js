05e88d27d5510c5542e7c15d36a5e9cb20b91199
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-07-20 12:46:11 +0300

    add api function logic to the books reducers

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+// import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
 const store = configureStore({
   reducer: {
-    booksReducer,
+    bookList: booksReducer,
     categoriesReducer,
   },
 });
